9e49c91b10b8510efa42f28b3dea0d6ccd9d06b9
Тимофей Курохтин <forgwent@gmail.com>
2021-02-21 23:44:46 +0300

    add pg gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,3 @@
 class Comment < ApplicationRecord
     validates :body,:author_id, presence: true
-    belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
     validates :title, :body, :author_id,  presence: true
-    has_many :comments
     paginates_per 5
 end
