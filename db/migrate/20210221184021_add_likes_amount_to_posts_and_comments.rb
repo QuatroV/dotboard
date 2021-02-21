847f3b55595f9c87107035de4ba16a55d2b24543
Тимофей Курохтин <forgwent@gmail.com>
2021-02-22 01:18:01 +0300

    fix size of posts on mobile

--- db/migrate/20210221184021_add_likes_amount_to_posts_and_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddLikesAmountToPostsAndComments < ActiveRecord::Migration[6.0]
-  def change
-    add_column :posts, :likes_count, :integer, :default => 0
-    add_column :comments, :likes_count, :integer, :default => 0
-  end
-end
