911b4f26e8b97a75149e0fe0515aa66881f0c42e
Тимофей Курохтин <forgwent@gmail.com>
2021-02-22 22:35:50 +0300

    Add avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def user_avatar(user)
+        if user.avatar.attached? then
+            user.avatar
+        else
+            "/default_avatar.jpg"
+        end
+    end
 end
