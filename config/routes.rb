0b56839f1c9c75aa5b098b5c675b9ffc9fbe62e2
Тимофей Курохтин <forgwent@gmail.com>
2021-02-23 19:13:50 +0300

    Wrong route lead to index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "posts/my_posts"
   get "posts/discussed_posts"
   resources :posts
+  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
